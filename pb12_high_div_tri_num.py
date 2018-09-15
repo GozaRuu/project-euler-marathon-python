4a99b9dc6bfe006799ea93970285d08347c03cd6
GozaRuu <kais.sghari@gmail.com>
2018-09-15 18:59:19 +0100

    updated pb12

--- pb12_high_div_tri_num.py
+++ pb12_high_div_tri_num.py
@@ -8,7 +8,7 @@ def ans(n):
 	for i in xrange(1, div_limit):
 		for j in xrange(i, div_limit, i):
 			d[j]+= 1 #add 1 to every multiple of i
-		c = d[i-1] * d[i//2] if i % 2 == 0 else d[(i-1)//2] * d[i] #sum the divisors from the array using gauss formula, d[i] or d[i-1] is even for sure
+		c = d[i-1] * d[i//2] if i % 2 == 0 else d[(i-1)//2] * d[i] #the idea is that the number of dividors of n = i*(i-1)/2 is d[n] = d[i*(i-1)/2] = d[i] * d[(i-1)/2]
 		if c > n: return i*(i-1)//2 #return the triangle number using gauss formula
 
 
