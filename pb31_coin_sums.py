a80a5a9f59665e304301a4b43fab065963f16239
GozaRuu <kais.sghari@gmail.com>
2018-09-24 00:36:12 +0100

    updated pb31 solution to be faster, more general as a template

--- pb31_coin_sums.py
+++ pb31_coin_sums.py
@@ -7,15 +7,15 @@
 
 mod = 10**9 + 7
 coins = [1, 2, 5, 10, 20, 50, 100, 200]
-memo = [[-1 for i in xrange(8)] for j in xrange(100005)]
-for i in xrange(8):
-    memo[0][i] = 1
+memo = [[-1 for i in xrange(8)] for j in xrange(10**5 + 1)]
 
 def ans(n,m):
+    if n == 0: return 1
+    if m == 0: return 1 #if coins[0] != 1 return n % coins[0] == 0 ? 1:0
     if memo[n][m] == -1:
         memo[n][m] = 0
-        if m > 0: memo[n][m] += ans(n, m-1)
-        if n >= coins[m]: memo[n][m] += ans(n-coins[m], m)
+        for coin_index in reversed(xrange(0,m+1)): #use reversed array to take the biggest coin first to avoid going deeper than the maximum recursive calls limit when you start by 1
+            if n >= coins[coin_index]: memo[n][m] += ans(n-coins[coin_index], coin_index) % mod
     return memo[n][m] % mod
 
 
